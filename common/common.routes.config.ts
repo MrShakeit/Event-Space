aba768f5784c132558d9c65bb52d5deb0007f1dc
MrShakeit <pmazurkiewicz01@gmail.com>
2023-03-11 17:29:11 +0100

    defining base rout config class

---
+++ common/common.routes.config.ts
@@ -0,0 +1,17 @@
+import express from "express";
+export class CommonRoutesConfig {
+  app: express.Application;
+  name: string;
+
+  Constructor(app: express.Application, name: string) {
+    this.app = app;
+    this.name = name;
+  }
+
+  getName() {
+    return this.name;
+  }
+}
+//defining class with 3 properties app(instance of express app) name(of route configuration) and constructor method(takes 2 parameters)
+
+//thanks to this class other classes can inherit app and name properties and acces getName method
