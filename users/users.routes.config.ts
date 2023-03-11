a5cbbacedea367427bf4ee7292fd1178201ca71d
MrShakeit <pmazurkiewicz01@gmail.com>
2023-03-11 18:02:41 +0100

    added abstract function

--- users/users.routes.config.ts
+++ users/users.routes.config.ts
@@ -5,6 +5,10 @@ export class UsersRoutes extends CommonRoutesConfig {
   constructor(app: express.Application) {
     super(app, "UsersRoutes");
   }
+
+  configureRoutes() {
+    return this.app;
+  }
 }
 
 //defines class which is extension to commonroutesconfig, has constructor method which has super method(by calling it usersroutes is inheriting properties and methods from common... and sets name of common... class to usersroutes)
